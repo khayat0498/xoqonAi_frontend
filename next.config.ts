457d369c7c9d9eb376a5d0bcd1092650c4b37808
khayat0498 <hayotbek040498@gmail.com>
2026-04-24 02:16:14 +0500

    logo width:100%

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
   turbopack: {
     root: __dirname,
+    resolveAlias: {
+      tailwindcss: path.resolve(__dirname, "node_modules/tailwindcss"),
+    },
+  },
+  webpack(config) {
+    config.resolve.modules = [
+      path.resolve(__dirname, "node_modules"),
+      "node_modules",
+    ];
+    return config;
   },
 };
 
